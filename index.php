67daea7037a53de746b867d85e337f0ad0c1288a
fran1380 <jaguar13xxx@gmail.com>
2014-05-23 22:12:05 -0500

    slide

--- index.php
+++ index.php
@@ -1,4 +1,41 @@
 <?php get_template_part('templates/page', 'header'); ?>
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="http://placehold.it/1140x300/FF530D/FFFFFF" alt="Slide1">
+      <div class="carousel-caption">
+        Slide 1
+      </div>
+    </div>
+    <div class="item">
+      <img src="http://placehold.it/1140x300/FF0000/FFFFFF" alt="Slide2">
+      <div class="carousel-caption">
+        Slide 2
+      </div>
+    </div>
+    <div class="item">
+      <img src="http://placehold.it/1140x300/FF0DFF/FFFFFF" alt="Slide3">
+      <div class="carousel-caption">
+        Slide 3
+      </div>
+    </div>
+  </div>
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+  </a>
+</div>
 
 <?php if (!have_posts()) : ?>
   <div class="alert alert-warning">
