5d67ae2af7a641bc3103457cfd81ea3db278070b
eBaeza <ebaeza92@hotmail.com>
2014-05-23 22:16:58 -0500

    carousel colores fuente karat

--- index.php
+++ index.php
@@ -1,5 +1,45 @@
 <?php get_template_part('templates/page', 'header'); ?>
 
+
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="http://placehold.it/1140x300/F2CC67/FFFFFF" alt="...">
+      <div class="carousel-caption">
+        Lorem ipsum dolor sit amet.
+      </div>
+    </div>
+    <div class="item">
+      <img src="http://placehold.it/1140x300/228DFF/FFFFFF" alt="...">
+      <div class="carousel-caption">
+        Lorem ipsum dolor sit amet.
+      </div>
+    </div>
+    <div class="item">
+      <img src="http://placehold.it/1140x300/FF0092/FFFFFF" alt="...">
+      <div class="carousel-caption">
+        Lorem ipsum dolor sit amet.
+      </div>
+    </div>
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+  </a>
+</div>
+
 <?php if (!have_posts()) : ?>
   <div class="alert alert-warning">
     <?php _e('Sorry, no results were found.', 'roots'); ?>
