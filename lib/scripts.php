446dcef98f35e5c8ee871d7f0181656cd9247056
fran1380 <jaguar13xxx@gmail.com>
2014-05-23 22:54:55 -0500

    nuevas fuentes (EpocaClassicLight, ImparaLig, MrJonesBook, PancettaProRegular, SinkinSans)

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '4733b9487c39f27d23f5d1f33ef699cc');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f5cb6ac226579b4e11a68605baccd940');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
